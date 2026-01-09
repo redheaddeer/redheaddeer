3d72d82f9551b9f903da7116b965304ce1cd8208
Ксения Тапок <soulssfg@gmail.com>
2026-01-09 21:49:18 +0300

    init
    init

---
+++ first/src/main/kotlin/ru/redhead/reed/Main.kt
@@ -0,0 +1,12 @@
+package ru.redhead.reed
+
+class Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        java.lang.System.out.printf("Hello and welcome!")
+
+        for (i in 1..5) {
+            println("i = " + i)
+        }
+    }
+}
