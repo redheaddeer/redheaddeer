60855b3f2a4a8edeb53c5a3720029d0d38f6956d
Ксения Тапок <soulssfg@gmail.com>
2026-01-14 18:32:58 +0300

    mini upd

--- first/src/main/kotlin/red/head/deer/first/controller/SimpleController.kt
+++ first/src/main/kotlin/red/head/deer/first/controller/SimpleController.kt
@@ -2,13 +2,14 @@ package red.head.deer.first.controller
 
 import jakarta.servlet.http.HttpServletResponse
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
 import red.head.deer.first.service.SimpleService
 
+@RestController
 class SimpleController {
 
     @PostMapping("/start")
     fun start(): Any {
-        println("Start test")
         SimpleService.KLogger.startService()
         return HttpServletResponse.SC_FOUND
     }
---
+++ first/src/main/kotlin/red/head/deer/first/rest/RestSpecification.kt
@@ -0,0 +1,8 @@
+package red.head.deer.first.rest
+
+import org.springframework.web.client.RestClient
+import org.springframework.web.client.RestClient.*
+
+class RestSpecification {
+
+}
