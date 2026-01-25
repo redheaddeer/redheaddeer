457ded417acaf910b979d8afd464da928288cb81
Ксения Тапок <soulssfg@gmail.com>
2026-01-25 22:04:57 +0300

    Добавим вывод карты

--- oracle-app/src/main/resources/static/scripts/left-buttons.js
+++ oracle-app/src/main/resources/static/scripts/left-buttons.js
@@ -2,6 +2,7 @@ document.getElementById('btn-day-card').addEventListener('click', async () => {
     try {
         const response = await fetch('/day-prophecy', {method: 'GET'});
         const result = await response.text();
+        document.getElementById('tarot-image').src = '/img/cards/Tarot.png';
         document.getElementById('output').innerHTML = result;
     } catch (error) {
         console.error('Ошибка:', error.message);
@@ -14,6 +15,7 @@ document.getElementById('btn-day-rune').addEventListener('click', async () => {
         const response = await fetch('/rune-prophecy', {method: 'GET'});
         const result = await response.text();
         document.getElementById('output').innerHTML = result;
+        document.getElementById('tarot-image').src = '/img/cards/Tarot.jpg';
     } catch (error) {
         console.error('Ошибка:', error.message);
         document.getElementById('output').innerHTML = '<span style="color: red;">Ошибка отправки данных!</span>';
