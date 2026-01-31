f0312883e5f3ebdfb10060a0ae36767602a3cc84
Ксения Тапок <soulssfg@gmail.com>
2026-01-31 22:36:34 +0300

    красивый вывод текста

--- oracle-app/src/main/resources/static/scripts/left-buttons.js
+++ oracle-app/src/main/resources/static/scripts/left-buttons.js
@@ -1,12 +1,25 @@
 document.getElementById('btn-day-card').addEventListener('click', async () => {
+    document.getElementById('output').innerHTML = "";
+    let index = 0;
+    const typingElement = document.getElementById('output');
     try {
         const response = await fetch('/day-prophecy', {method: 'GET'});
         const result = await response.text();
         document.getElementById('tarot-image').src = '/img/cards/Tarot.png';
-        document.getElementById('output').innerHTML = result;
+        text = result;
+        typeWriter();
     } catch (error) {
         console.error('Ошибка:', error.message);
         document.getElementById('output').innerHTML = '<span style="color: red;">Ошибка отправки данных!</span>';
+    }
+
+    // Функция для добавления следующего символа
+    function typeWriter() {
+      if (index < text.length) {
+        typingElement.textContent += text.charAt(index);
+        index++;
+        setTimeout(typeWriter, 50); // задержка между появлением символов (можно регулировать)
+      }
     }
 });
 
